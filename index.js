05423c4ef3f3a9b8750ed33d75a3a44d47db38f2
vknayak <kumar19@navgurukul.org>
2020-09-01 08:07:37 +0530

    added tracking days

--- index.js
+++ index.js
@@ -28,5 +28,14 @@ const NG_CONSTANTS = {
       "dumbeldore",
     ],
   },
+  trackingDays: {
+    "0": "Sunday",
+    "1": "Monday",
+    "2": "Tuesday",
+    "3": "Wednesday",
+    "4": "Thursday",
+    "5": "Friday",
+    "6":"Saturday",
+  },
 };
 module.exports = NG_CONSTANTS
